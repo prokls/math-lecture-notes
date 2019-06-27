766036cbbb7af040ec0c02cfb58430a9f50d7e59
meisterluk <admin@lukas-prokop.at>
2019-06-27 12:43:26 +0200

    Update 2019-06-27

--- optimization/compile.sh
+++ optimization/compile.sh
@@ -6,5 +6,6 @@ if [ -f "notes.idx" ]; then
   makeindex notes
 fi
 xelatex -halt-on-error --synctex=1 -interaction errorstopmode theorems_and_definitions
+xelatex -halt-on-error --synctex=1 -interaction errorstopmode theorems_and_definitions_2
 xelatex -halt-on-error --synctex=1 -interaction errorstopmode notes
 exit "$?"
